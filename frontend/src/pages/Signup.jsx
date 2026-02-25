0f2ab4ddf86cbf3178b324a5cc5f9697e85545bd
StevenCPY <scpy21@gmail.com>
2026-02-25 05:40:31 -0500

    Sign Up button only enabled if username, email and password non-empty

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Signup() {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
         <form>
             <div style={{
@@ -10,16 +15,29 @@ function Signup() {
                 marginRight: "auto",
                 width: "15rem",
                 height: "20rem"
-
             }}>
                 <span style={{ marginLeft: "auto", marginRight: "auto" }}>SIGN UP</span>
                 <label>Username:</label>
-                <input type="text" />
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
                 <label>Email:</label>
-                <input type="text" />
+                <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
                 <label>Password:</label>
-                <input type="text" />
-                <button disabled>Sign up</button>
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                {/* show this text only if username, email or password is blank */}
+                {!(username && email && password) && <div style={{ color:"red" }} >Fill in all fields!</div>}
+                <button disabled={!(username && email && password)}>Sign up</button>
                 <span>
                     Already have an account?{" "}
                     <Link to="/Login">Login</Link>

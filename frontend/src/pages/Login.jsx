f0fdc4d4a0aa58f091ea8fd37e6d68baf3cee07a
StevenCPY <scpy21@gmail.com>
2026-02-25 02:05:18 -0500

    Added simple sign up/login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,32 @@
+import { Link } from 'react-router-dom'
+
 function Login() {
     return (
-        <div>
-        </div>
+        <form>
+            <div style={{
+                display:"flex",
+                flexDirection: "column",
+                marginLeft: "auto",
+                marginRight: "auto",
+                width: "15rem",
+                height: "20rem"
+
+            }}>
+                <span style={{ marginLeft: "auto", marginRight: "auto" }}>LOGIN</span>
+                <label>Username:</label>
+                <input type="text" />
+                <label>Password:</label>
+                <input type="text" />
+                <button disabled>Login</button>
+                <span>
+                    Don't have an account?{" "}
+                    <Link to="/Signup">Sign up</Link>
+                </span>
+
+            </div>
+            
+
+        </form>
     )
 }
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,33 @@
+import { Link } from 'react-router-dom'
+
 function Signup() {
     return (
-        <div>
-        </div>
+        <form>
+            <div style={{
+                display:"flex",
+                flexDirection: "column",
+                marginLeft: "auto",
+                marginRight: "auto",
+                width: "15rem",
+                height: "20rem"
+
+            }}>
+                <span style={{ marginLeft: "auto", marginRight: "auto" }}>SIGN UP</span>
+                <label>Username:</label>
+                <input type="text" />
+                <label>Email:</label>
+                <input type="text" />
+                <label>Password:</label>
+                <input type="text" />
+                <button disabled>Sign up</button>
+                <span>
+                    Already have an account?{" "}
+                    <Link to="/Login">Login</Link>
+                </span>
+
+            </div>
+
+        </form>
     )
 }
 

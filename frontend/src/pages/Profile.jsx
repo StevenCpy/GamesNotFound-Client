4775b18fb653a46e44c748926ef201c1decc0086
StevenCPY <scpy21@gmail.com>
2026-05-14 19:10:32 -0400

    Added logout button on Profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,29 @@
+import { useContext } from "react"
+import { AuthContext } from "../components/Context"
+import { useNavigate } from "react-router-dom"
+
 function Profile() {
+    const { currentUser, setCurrentUser } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    function handleLogout() {
+        setCurrentUser(null)
+        navigate("/")
+        // clear JWT cookie + tell server that user logged out so it can invalidate token
+    }
+
     return (
-        <div>
+        <div
+        style={{
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "50rem",
+            height: "20rem",
+            fontSize: "150%",
+            textAlign: "center"
+        }}>
+            {currentUser}<br></br>
+            <button onClick={ handleLogout }>Log out</button>
         </div>
     )
 }
